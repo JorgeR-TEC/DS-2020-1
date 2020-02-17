84220371ef2b33a65cf83780a91c4535d2931cd7
Jorge <jorger@tec.mx>
2020-02-17 14:35:18 -0600

    Agregados tipos

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
+
 int main(){
     cout<<"Hello World!"<<endl;
+    int a=1;
+    double b=1.1;
+    char c='a';//esto es un caracter
+    string d="hola";
+    cout<<"Dame un numero: ";
+    cin>>a;
+    cout<<"El numero dado fue: "<<a<<endl;
+    return 0;
 }
