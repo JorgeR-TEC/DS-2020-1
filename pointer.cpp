461c9881a6e9f10cb90aeee58afed33088542137
Jorge <jorger@tec.mx>
2020-02-20 14:15:16 -0600

    Agregado codigo de apuntadores

---
+++ pointer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=56;
+    int *b=&a;
+    int c[5]={10,20,30,40,50};
+    int *d=c;
+    int *e=new int(5);
+    cout<<*(c+2)<<endl;
+}
